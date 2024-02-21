49cbbc6459a1d058dca3a1cf47a81d999e3d7692
Hojin Kim <miu.null@gmail.com>
2024-02-22 01:16:44 +0900

    validation setting

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // filtering except property that defined in dto file
+      forbidNonWhitelisted: true, // send error message if the input value is not defined in dto
+      transform: true, // cast the data type
+    }),
+  );
+
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
