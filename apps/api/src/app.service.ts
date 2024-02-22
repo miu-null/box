3b8c00789a238b85966eaceb2ad6b58b20297b58
Hojin Kim <miu.null@gmail.com>
2024-02-23 01:02:07 +0900

    add BEcode to saving DB

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private comments = [];
+  // getHello(): string {
+  //   return 'Hello World!';
+  // }
+  async writeComment(Account: string, Comment: string, Date: string) {
+    const commentId = this.comments.length + 1;
+    //const writeComment = await
+    this.comments.push({ id: commentId, Account, Comment, Date });
+    return Comment;
   }
 }
